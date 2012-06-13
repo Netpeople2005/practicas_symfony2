5c2ec0f6eb8a1ea04e49efdd07c3b1a9b87f2456
Alberto <arp2312@gmail.com>
2012-06-13 16:36:47 -0430

    configs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Manuel\InicioBundle\ManuelInicioBundle(),
             new Manuel\PersonasBundle\ManuelPersonasBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
